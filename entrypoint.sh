5d203148235bda99d5cd616e2f024af8f2e67602
umi <9@umi.cat>
2021-05-17 20:52:03 +0800

    uodate

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ chia init
 
 chia init -c /ca
 
-mv /opt/conf/config.yaml ~/.chia/mainnet/config/config.yaml
-#sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
+cp /opt/conf/config.yaml ~/.chia/mainnet/config/config.yaml
+sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
 
 chia start harvester
 
